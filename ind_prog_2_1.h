fe7a0429c726a9782bcf951847a61a478571ff95
ZovuPitPivo <2146376@gmail.com>
2023-02-23 02:53:05 +0300

    ind_prog

---
+++ ind_prog_2_1.h
@@ -0,0 +1,12 @@
+#pragma once
+class circle
+{
+private:
+	float R, x, y;
+public:
+	bool exst_tr();
+	void set(double a1, double b1, double c1);
+	void show();
+	double perimetr();
+	double square();
+};
