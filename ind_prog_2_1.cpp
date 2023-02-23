d4663b92751777a5191ba279de5c4e0fdedf464e
ZovuPitPivo <125370353+ZovuPitPivo@users.noreply.github.com>
2023-02-23 21:10:17 +0300

    Update ind_prog_2_1.cpp

--- ind_prog_2_1.cpp
+++ ind_prog_2_1.cpp
@@ -10,16 +10,16 @@ void tringle::set(a1, b1, c1) {
 	b = b1;
 	c = c1;
 }
-void tringle::show() {
+void tringle::show(double a, double b, double c) {
 	std::cout << "1 сторона: " << a << endl;
 	std::cout << "2 сторона: " << b << endl;
 	std::cout << "3 сторона: " << c << endl;
 }
-double tringle::perimetr() {
+double tringle::perimetr(double a, double b, double c) {
 	double P = a + b + c;
 	return P;
 }
-double tringle::square() {
+double tringle::square(double a, double b, double c) {
 	double p = (a + b + c) / 2;
 	double S = sqrt(p * (p - a) * (p - b) * (p - c));
 	return S;
