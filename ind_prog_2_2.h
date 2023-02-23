f0368ebae9392b1709cc513f7def9e27d3199713
ZovuPitPivo <125370353+ZovuPitPivo@users.noreply.github.com>
2023-02-23 15:29:28 +0300

    Update ind_prog_2_2.h

--- ind_prog_2_2.h
+++ ind_prog_2_2.h
@@ -1,12 +1,15 @@
-class circle
-{
+#pragma once
+
+class circle{
 private:
-	float R, x, y;
+	float R, float x, float y;
 public:
 	circle(float r, float x, float y);
+	circle();
 	void set_cercle(float x1, float y1, float R1);
-	float square();
-	bool tringle_around();
-	bool tringle_in();
-	bool check_circle();
+	float square(float R);
+	bool tringle_around(float a, float b, float c);
+	bool tringle_in(float a, float b, float c);
+	bool check_circle(float x_cntr, float y_cntr);
 };
+
