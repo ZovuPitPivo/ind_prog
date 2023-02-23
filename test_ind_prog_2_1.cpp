b1e87afc275d53767899efbbc990104ebef52901
ZovuPitPivo <125370353+ZovuPitPivo@users.noreply.github.com>
2023-02-23 21:05:52 +0300

    Update test_ind_prog_2_1.cpp

--- test_ind_prog_2_1.cpp
+++ test_ind_prog_2_1.cpp
@@ -1,20 +1,26 @@
+#include <iostresm>
+#include "ind_prog_2_1.h"
+
 int main() {
-	triange mas[3];
+	setlocale(LC_ALL, "Russian");
+	setlocale(LC_NUMERIC, "en_EN.utf8");
+
+	tringle mas[3];
 	double a, b, c;
 	for (int i = 0; i < 3; i++) {
-		cout << "Ââåäèòå a, b, c äëÿ òðåóãîëüíèêà ¹  " << i + 1 << " ÷åðåç ïðîáåë: " << endl;
-		cin >> a >> b >> c;
+		std :: cout << "Ð’Ð²ÐµÐ´Ð¸Ñ‚Ðµ a, b, c Ð´Ð»Ñ Ñ‚Ñ€ÐµÑƒÐ³Ð¾Ð»ÑŒÐ½Ð¸ÐºÐ° â„–  " << i + 1 << " Ñ‡ÐµÑ€ÐµÐ· Ð¿Ñ€Ð¾Ð±ÐµÐ»: " << endl;
+		std :: cin >> a >> b >> c;
 		mas[i].set(a, b, c);
 		if (!(mas[i].exst())) {
 			mas[i].show();
-			cout << "Òðåóãîëüíèê ñ òàêèìè ñòîðîíàìè íå ñóùåñòâóåò, ïîïðîáóéüå åùå ðàç" << endl;
+			std :: cout << "Ð¢Ñ€ÐµÑƒÐ³Ð¾Ð»ÑŒÐ½Ð¸Ðº Ñ Ñ‚Ð°ÐºÐ¸Ð¼Ð¸ ÑÑ‚Ð¾Ñ€Ð¾Ð½Ð°Ð¼Ð¸ Ð½Ðµ ÑÑƒÑ‰ÐµÑÑ‚Ð²ÑƒÐµÑ‚, Ð¿Ð¾Ð¿Ñ€Ð¾Ð±ÑƒÐ¹ÑŒÐµ ÐµÑ‰Ðµ Ñ€Ð°Ð·" << endl;
 			i--;
 		}
 	}
 	for (int i = 0; i < 3; i++) {
 		mas[i].show();
-		cout << "Ïåðèìåòð òðåóãîëüíèêà: " << mas[i].perimeter() << endl;
-		cout << "Ïëîùàäü òðåóãîëüíèêà: " << mas[i]d.square() << endl;
+		std :: cout << "ÐŸÐµÑ€Ð¸Ð¼ÐµÑ‚Ñ€ Ñ‚Ñ€ÐµÑƒÐ³Ð¾Ð»ÑŒÐ½Ð¸ÐºÐ°: " << mas[i].perimeter() << endl;
+		std :: cout << "ÐŸÐ»Ð¾Ñ‰Ð°Ð´ÑŒ Ñ‚Ñ€ÐµÑƒÐ³Ð¾Ð»ÑŒÐ½Ð¸ÐºÐ°: " << mas[i]d.square() << endl;
 	}
 	return 0;
 }
