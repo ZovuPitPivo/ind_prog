246485f62d867f25c3b8e79c2c1732d08fcd98aa
ZovuPitPivo <2146376@gmail.com>
2023-02-24 14:00:47 +0300

    Update test_ind_prog_2_1.cpp

--- test_ind_prog_2_1.cpp
+++ test_ind_prog_2_1.cpp
@@ -1,20 +1,26 @@
+#include <iostresm>
+#include "ind_prog_2_1.h"
+
 int main() {
-	triange mas[3];
+	setlocale(LC_ALL, "Russian");
+	setlocale(LC_NUMERIC, "en_EN.utf8");
+
+	tringle mas[3];
 	double a, b, c;
 	for (int i = 0; i < 3; i++) {
-		cout << "Введите a, b, c для треугольника №  " << i + 1 << " через пробел: " << endl;
-		cin >> a >> b >> c;
+		std :: cout << "Введите a, b, c для треугольника №  " << i + 1 << " через пробел: " << endl;
+		std :: cin >> a >> b >> c;
 		mas[i].set(a, b, c);
 		if (!(mas[i].exst())) {
 			mas[i].show();
-			cout << "Треугольник с такими сторонами не существует, попробуйье еще раз" << endl;
+			std :: cout << "Треугольник с такими сторонами не существует, попробуйье еще раз" << endl;
 			i--;
 		}
 	}
 	for (int i = 0; i < 3; i++) {
 		mas[i].show();
-		cout << "Периметр треугольника: " << mas[i].perimeter() << endl;
-		cout << "Площадь треугольника: " << mas[i]d.square() << endl;
+		std :: cout << "Периметр треугольника: " << mas[i].perimeter() << endl;
+		std :: cout << "Площадь треугольника: " << mas[i]d.square() << endl;
 	}
 	return 0;
 }
