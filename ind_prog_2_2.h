f918f30d675dbb7d0f31f99e2285dd334dd5cccd
ZovuPitPivo <125370353+ZovuPitPivo@users.noreply.github.com>
2023-02-26 16:39:23 +0300

    Update ind_prog_2_2.h

--- ind_prog_2_2.h
+++ ind_prog_2_2.h
@@ -7,7 +7,7 @@ public:
 	circle(float r, float x, float y);
 	circle();
 	void set_cercle(float x1, float y1, float R1);
-	float square(float R);
+	float square();
 	bool tringle_around(float a, float b, float c);
 	bool tringle_in(float a, float b, float c);
 	bool check_circle(float x_cntr, float y_cntr);
