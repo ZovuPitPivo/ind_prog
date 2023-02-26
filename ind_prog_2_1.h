0135d000aa594548669fd850f8b61bd054494a75
ZovuPitPivo <125370353+ZovuPitPivo@users.noreply.github.com>
2023-02-26 17:35:17 +0300

    Update ind_prog_2_1.h

--- ind_prog_2_1.h
+++ ind_prog_2_1.h
@@ -4,7 +4,7 @@ class tringle
 private:
 	float a, b, c;
 public:
-	bool exst_tr(double a, double b, double c);
+	bool exst_tr();
 	void set(double a1, double b1, double c1);
 	void show();
 	double perimetr();
