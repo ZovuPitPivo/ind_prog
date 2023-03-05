a7ec14ea6da4d6b7a522dcdc9715e865b4f8938d
ZovuPitPivo <2146376@gmail.com>
2023-03-06 01:06:34 +0300

    ind_prog

---
+++ Практика 2/Source1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+#include "Header1.h"
+
+bool tringle::exst_tr(double a, double b, double c) {
+	return ((a + b > c) && (a + c > b) && (b + c > a));
+}
+void tringle::set(a1, b1, c1) {
+	a = a1;
+	b = b1;
+	c = c1;
+}
+void tringle::show() {
+	std::cout << "1 сторона: " << a << endl;
+	std::cout << "2 сторона: " << b << endl;
+	std::cout << "3 сторона: " << c << endl;
+}
+double tringle::perimetr() {
+	double P = a + b + c;
+	return P;
+}
+double tringle::square() {
+	double p = (a + b + c) / 2;
+	double S = sqrt(p * (p - a) * (p - b) * (p - c));
+	return S;
+}
---
+++ Практика 2/test_ind_prog_2_2.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "Header2.h"
+
+int main() {
+	circle mas[3];
+}
